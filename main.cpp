1524e38d443ac766b417be88b664a9a061dc2e98
nepaulius <pauliuscern.lt@gmail.com>
2019-06-04 10:52:07 +0300

    5

--- main.cpp
+++ main.cpp
@@ -10,87 +10,53 @@
 #include <algorithm>
 #include <regex>
 #include <bits/stdc++.h>
-#include <locale>
+#include <set>
 
- void URL_www(std::string tekastas)
+ void URL(std::string tekastas)
  {
-  std::string w="www.";
-  std::vector<size_t> positions;
 
-size_t pos = tekastas.find(w, 0);
-while(pos != std::string::npos)
-{
-    positions.push_back(pos);
-    pos = tekastas.find(w,pos+1);
-}
+    transform(tekastas.begin(),tekastas.end(),tekastas.begin(),::tolower);
+    std::string word;
+    std::set<std::string> Linkai;
+    std::istringstream iss(tekastas);
 
-std::string sum;
-for(int i=0;i<positions.size();i++)
-{
-        if(tekastas[positions[i]-1]==' ')
-        {
-            for(int j=positions[i];j<tekastas.size();j++)
-        {
-            if(!isspace(tekastas[j]))
-            sum+=tekastas[j];
-            else break;
-
+    std::cout<<std::endl;
+    std::cout<<"Link'ai tekste : "<<std::endl;
+            while (iss>>word){
+            if(std::regex_match(word, std::regex("[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")) )
+           {
+                Linkai.insert(word);
+           }
         }
-sum+=" ";
-}
-}
-for(int i=0;i<sum.size();i++)
-    if (sum[i]==',')
-      sum.erase(i);
+ for(std::set<std::string>::iterator it = Linkai.begin(); it != Linkai.end(); ++it) {
+        std::cout << *(it) << std::endl;
+    }
 
-std::cout<<sum<<std::endl;
+
  }
-void  URL_http(std::string tekastas)
+
+int main()
 {
 
+std::ifstream in("my_data.txt");
 
-  std::string w="http";
-  std::vector<size_t> positions;
 
-size_t pos = tekastas.find(w, 0);
-while(pos != std::string::npos)
+   if(!in.good()) {
+    std::cout << "Nepavyko atidaryti failo" << std::endl;
+    }
+
+
+std::map <std::string,std::vector<int> > vieta;
+std::string sLine;
+std::string tmp;
+std::string str;
+std::string laikin;
+
+int count=0;
+while (std::getline(in,sLine))
 {
-    positions.push_back(pos);
-    pos = tekastas.find(w,pos+1);
-}
-
-std::string sum;
-for(int i=0;i<positions.size();i++)
-{
-
-        for(int j=positions[i];j<tekastas.size();j++)
-        {
-            if(!isspace(tekastas[j]))
-            sum+=tekastas[j];
-            else break;
-
-        }
-sum+=" ";
-}
-for(int i=0;i<sum.size();i++)
-    if (sum[i]==',')
-      sum.erase(i);
-std::cout<<sum<<std::endl;
- }
-void Vieta(std::vector<std::string> geri)
-{
-    int counter=0;
-    std::string word2;
-    std::string sLine;
-    std::string plius;
-    std::ifstream in("my_data.txt");
-
-    std::vector<std::string> line;
-
-
-while(std::getline(in,sLine))
-{
-     for (int i = 0; i < sLine.size(); i++)
+    str += sLine + "\n";
+for (int i = 0; i < sLine.size(); i++)
     {
 
     transform(sLine.begin(),sLine.end(),sLine.begin(),::tolower);
@@ -100,85 +66,28 @@ while(std::getline(in,sLine))
             i--;
         }
     }
-     line.push_back(sLine);
-}
-std::string pvz;
-
-for(int i=0;i<line.size();i++)
-{
-    std::istringstream b(line[i]);
-    while ( b >>pvz)
+    std::istringstream b(sLine);
+    while ( b >>tmp)
     {
-        for(int j=0;j<geri.size();j++)
+        vieta[tmp].push_back(count);
+    }
+    count++;
+}
+
+ for( const auto& pair : vieta )
+    {
+        if(pair.second.size()>1)
         {
-            if(geri[j]==pvz)
-            {
-                std::cout<<geri[j]<<" yra eiluteje : "<<i+1<<std::endl;
-            }
+        std::cout <<"Zodis : "<< pair.first << " pasikartoja : "<<pair.second.size()<<" kartus ir yra " ;
+        for( int n : pair.second ) std::cout << n+1 <<" " ;
+        std::cout << "eilutese"<<std::endl ;
         }
     }
 
-}
+    URL(str);
 
-}
-int main()
-{
+    in.close();
 
-setlocale(LC_ALL, "Lithuanian");
-
- std::string str;
-  std::string tmp;
-  std::ifstream in("my_data.txt");
-
-while ( std::getline(in, tmp) )
-{
-    str += tmp + "\n";
-}
-
-    transform(str.begin(),str.end(),str.begin(),::tolower);
-    str.insert(0,1,' ');
-std::cout<<"Link'ai tekste : "<<std::endl;
-    URL_www(str);
-    URL_http(str);
-std::cout<<std::endl;
-    std::string word;
-
-    for (int i = 0; i < str.size(); i++)
-    {
-       if(iswpunct(str[i]) || isdigit(str[i]))
-        {
-            str.erase(i, 1);
-            i--;
-        }
-    }
-
-
-    std::istringstream iss(str);
-    std::map<std::string, std::size_t> occurrences;
-
-    while (iss >> word)
-    {
-        ++occurrences[word];
-    }
-
-std::vector <std::string> geri;
-    for (std::map<std::string, std::size_t>::iterator it = occurrences.begin();
-         it != occurrences.end(); ++it)
-    {
-        if(it->second>1)
-        {
-            std::cout << "Zodis: " << it->first << "\t pasikartoja : " << it->second<<" kartus"<< std::endl;
-            geri.push_back(it->first);
-        }
-
-
-    }
-std::cout<<std::endl;
-
-Vieta(geri);
-
-
-  in.close();
 
     return 0;
 }
